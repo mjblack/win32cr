0d831dce675212b9c1640e34f2ca59f90145cf73
Matthew J. Black <mjblack@gmail.com>
2023-06-17 09:32:27 -0500

    Fixed GUID to_s and to_clsid methods

--- src/win32cr.cr
+++ src/win32cr.cr
@@ -5,10 +5,10 @@ end
 
 struct LibC::GUID
   def to_s
-    sprintf("%08X-%04X-%04X-%02X%02X-%02X%02X%02X%02X%02X%02X", @data1, @data2, data3, @data4[0], @data4[1], @data4[2], @data4[3], @data4[4], @data4[5], @data4[6], @data4[7])
+    sprintf("%08X-%04X-%04X-%02X%02X-%02X%02X%02X%02X%02X%02X", @data1, @data2, @data3, @data4[0], @data4[1], @data4[2], @data4[3], @data4[4], @data4[5], @data4[6], @data4[7])
   end
   def to_clsid
-    sprintf("{%08X-%04X-%04X-%02X%02X-%02X%02X%02X%02X%02X%02X}", @data1, @data2, data3, @data4[0], @data4[1], @data4[2], @data4[3], @data4[4], @data4[5], @data4[6], @data4[7])
+    sprintf("{%08X-%04X-%04X-%02X%02X-%02X%02X%02X%02X%02X%02X}", @data1, @data2, @data3, @data4[0], @data4[1], @data4[2], @data4[3], @data4[4], @data4[5], @data4[6], @data4[7])
   end
 end
 
